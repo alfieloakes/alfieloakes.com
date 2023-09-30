68c5464a907df4a68fc48fe9a9bc900ae1c80fed
Alfie Loakes <alfieloakes@gmail.com>
2023-09-30 22:43:46 +0100

    Simple redesign (#2)
    * chore: renaming navbar component to be pascal case
    
    * Simple redesign

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -8,7 +8,7 @@ export default function Title({ title }: Props) {
   return (
     <header className="container px-4">
       <div className="grid grid-cols-1 my-6 sm:my-10 md:my-10 gap-4">
-        <h1 className="text-lg sm:text-4xl font-bold">{title}</h1>
+        <h1 className="text-lg sm:text-2xl font-bold">{title}</h1>
       </div>
     </header>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import About from './components/About';
 
 export default function Home() {
   return (
-    <div className="container px-4">
-      <div className="grid sm:grid-cols-12 my-4 sm:my-20 md:my-48 gap-4">
-        <div className="sm:col-start-2 sm:col-span-10">
-          <h1 className="text-lg sm:text-4xl font-bold sm:mb-8 mb-2">Hey, I&#39;m Alfie 👋 </h1>
-          <p className="font-light text-sm sm:text-2xl sm:font-regular sm:mb-8 mb-2">Welcome to my corner of the internet!</p>
-          <p className="text-base sm:text-3xl font-medium">I&#39;m a software engineer from the UK, working with Typescript, Golang and .NET</p>
-        </div>
-      </div>
+    <div>
+      <About />
     </div>
   );
 }
